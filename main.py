c698a8bb9b93422064c0ef26632bb985fe90a018
ingl3585 <ingleanthonyco@gmail.com>
2025-08-26 17:16:42 -0500

    aligning with master

--- main.py
+++ main.py
@@ -10,4 +10,4 @@ app = FastAPI(title=config.API_TITLE)
 app.include_router(api_router)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000) 
+    uvicorn.run(app, host="127.0.0.1", port=8000)
